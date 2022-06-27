8f6c670e0c9c260f9cd453cbc00117a49b2f8480
Karter <kdlmuzixnerd14@gmail.com>
2022-06-27 15:05:40 -0700

    commit tree 7522999d1467219cb47991e00aad6f1d71304832

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 4;
+let num2 = 15.5;
+let num3 = 20;
+let num4 = 8;
+
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(num1,num2,num3,num4);
+
